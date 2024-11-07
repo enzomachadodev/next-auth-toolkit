b1c0c5e836da93bcd985c857e6fbb422d89f2d4b
Enzo Machado <enzo653fm@gmail.com>
2024-11-06 22:37:59 -0300

    feat: create card wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="from sky-400 flex h-full w-full items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <main className="from sky-400 bg-blue-gradient flex h-full w-full items-center justify-center">
       <div className="space-y-6 text-center">
         <h1 className="text-6xl font-semibold text-white drop-shadow-md">
           Auth
---
+++ src/components/auth/header.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils";
+
+interface HeaderProps {
+  label: string;
+}
+export const Header = ({ label }: HeaderProps) => {
+  return (
+    <header className="flex w-full flex-col items-center justify-center gap-y-4">
+      <h1 className={cn("text-3xl font-semibold")}> Auth</h1>
+      <p className="text-sm text-muted-foreground">{label}</p>
+    </header>
+  );
+};
