d5aa5c22f88da5a29f204ef5ad89d336f656cf74
Enzo Machado <enzo653fm@gmail.com>
2024-11-15 14:32:23 -0300

    feat: create admin exemple server action

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -6,11 +6,23 @@ import { RoleGate } from "@/components/auth/role-gate";
 import { FormSuccess } from "@/components/form-success";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { toast } from "sonner";
+import { admin } from "@/actions/admin";
 
 const AdminPage = () => {
+  const handleServerActionClick = () => {
+    admin().then((data) => {
+      if (data.sucess) {
+        toast.success(data.sucess);
+      }
+
+      if (data.error) {
+        toast.success(data.error);
+      }
+    });
+  };
+
   const handleApiRouteClick = () => {
     fetch("/api/admin").then((response) => {
-      console.log(response);
       if (response.ok) {
         toast.success("Allowed API Route!");
       } else {
@@ -24,7 +36,7 @@ const AdminPage = () => {
       <CardHeader>
         <p className="text-center text-2xl font-semibold">Admin 🔑</p>
       </CardHeader>
-      <CardContent>
+      <CardContent className="space-y-4">
         <RoleGate allowedRole={UserRole.ADMIN}>
           <FormSuccess message="You are allowed to see this content!" />
         </RoleGate>
@@ -34,7 +46,7 @@ const AdminPage = () => {
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium">Admin-only Server Action</p>
-          <Button>Click to test</Button>
+          <Button onClick={handleServerActionClick}>Click to test</Button>
         </div>
       </CardContent>
     </Card>
