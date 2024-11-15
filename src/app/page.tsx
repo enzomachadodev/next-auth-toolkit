2191ac008792e8c8b449f26f3ce46bb718a0b2be
Enzo Machado <enzo653fm@gmail.com>
2024-11-15 19:01:56 -0300

    feat: add login modal option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         </h1>
         <p className="text-lg text-white">A simple authentication service</p>
         <div>
-          <LoginButton>
+          <LoginButton mode="modal" asChild>
             <Button variant="secondary" size="lg" className="font-medium">
               Sign in
             </Button>
