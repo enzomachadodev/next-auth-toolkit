314ad1b621910f341d21b4b274f0e39c9cac2c3b
Enzo Machado <enzo653fm@gmail.com>
2024-11-15 11:06:42 -0300

    feat: add emoji in header title

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
   return (
     <header className="flex w-full flex-col items-center justify-center gap-y-4">
-      <h1 className={cn("text-3xl font-semibold")}> Auth</h1>
+      <h1 className={cn("text-3xl font-semibold")}>Auth 🔐</h1>
       <p className="text-sm text-muted-foreground">{label}</p>
     </header>
   );
