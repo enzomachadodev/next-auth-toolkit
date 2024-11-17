bb4f9085ef1c0c4e5c87545ea6baac7105ecb949
Enzo Machado <enzo653fm@gmail.com>
2024-11-17 02:51:19 -0300

    refactor: update next-auth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -2,6 +2,7 @@ import type { DefaultSession } from "next-auth";
 import { UserRole } from "@prisma/client";
 
 type ExtendedUser = {
+  id: string;
   role: UserRole;
   isTwoFactorEnabled: boolean;
   isOAuth: boolean;
